7f12012dc450f60eb771e7e9045094f1f041cd11
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2026-03-15 22:14:43 +0100

    CH5 L3 done

--- src/main.py
+++ src/main.py
@@ -4,5 +4,9 @@ from func_generate_page import generate_page
 def main():
         copy_files("./static", "./public")
         generate_page("./content/index.md", "./template.html", "./public/index.html")
+        generate_page("./content/blog/glorfindel/index.md", "./template.html", "./public/blog/glorfindel/index.html")
+        generate_page("./content/blog/tom/index.md", "./template.html", "./public/blog/tom/index.html")
+        generate_page("./content/blog/majesty/index.md", "./template.html", "./public/blog/majesty/index.html")
+        generate_page("./content/contact/index.md", "./template.html", "./public/contact/index.html")
     
 main()
